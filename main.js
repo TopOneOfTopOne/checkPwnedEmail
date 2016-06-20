e2a9f3b752732012068f3a8d6a8329737e2927ed
toponeoftopone <nooblingis@gmail.com>
2016-06-20 21:15:42 +0800

    working for one email

--- main.js
+++ main.js
@@ -3,28 +3,30 @@ var domain = "https://haveibeenpwned.com/api/v2/breachedaccount/"
 
 $("#execute-btn").click(function() {
   var emails = $("#user-input").val();
-  var emailsArr = emails.split(/[.,\/ -]/);
+  var emailsArr = emails.split(/[,\/ -]/);
   checkEmails(emailsArr);
 })
 
 function checkEmails(emailsArr) {
   $.each(emailsArr, function(_, email) {
     var data;
+    var url = "https://haveibeenpwned.com/api/v2/breachedaccount/"+email
     $.ajax({
-      url: "https://haveibeenpwned.com/api/v2/breachedaccount/"+email,
-    }).done(function(json){data=json})
+      url: url
+    }).done(function(json){addHTML(json, email)})
       .fail(function(){alert("Failed for "+email)});
-    addHTML(data, email);
   })
 }
 
 function addHTML(data, email) {
-  buildEmailDiv(email);
+  var emailTitle = email;
+  email = email.replace(/[\.\@\#]/g,"");
+  buildEmailDiv(email, emailTitle);
   $.each(data, function(_, obj) {
     var website = obj.Name;
     buildScaffoldFor(website, obj.Domain, email);
     $.each(obj, function(klass, item) {
-      var selector = "#"+email+" ."+name+" ."+klass;
+      var selector = "#"+email+" ."+website+" ."+klass;
       if ($(selector).length) {
         $(selector).append(item)
       }
@@ -32,15 +34,15 @@ function addHTML(data, email) {
   })
 }
 
-function buildEmailDiv(email) {
+function buildEmailDiv(email, emailTitle) {
   $div = $("<div>", {id: email});
-  $emailTitle = $("<h1>", {text: email});
+  $emailTitle = $("<h1>", {text: emailTitle});
   $div.append($emailTitle);
   $("#body").append($div);
 }
 
 function buildScaffoldFor(website, domain, email) {
-  $div = $("<div>", {id: website, class: "panel panel-default panel-body"});
+  $div = $("<div>", {class: "panel panel-default panel-body " + website});
   $title = $("<h2>", {class: "Title"});
   $domain = $("<a>", {href: domain})
   $count = $("<p>", {class: "PwnCount", text: "# Breached accounts: ", id: "numBreach"});
