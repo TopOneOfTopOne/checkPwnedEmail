1ee95236c3ba1a7d9b34c4ff5dd225e279d0b774
toponeoftopone <nooblingis@gmail.com>
2016-06-20 20:47:26 +0800

    finish adding basic code (not working)

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
 var domain = "https://haveibeenpwned.com/api/v2/breachedaccount/"
 
-$.ajax({
-  url: "https://haveibeenpwned.com/api/v2/breachedaccount/test@example.com",
 
-}).done(handleRes)
-  .fail(function(){alert("Failed")});
 $("#execute-btn").click(function() {
   var emails = $("#user-input").val();
   var emailsArr = emails.split(/[.,\/ -]/);
-  alert(JSON.stringify(emailsArr))
+  checkEmails(emailsArr);
 })
-function handleRes(data) {
+
+function checkEmails(emailsArr) {
+  $.each(emailsArr, function(_, email) {
+    var data;
+    $.ajax({
+      url: "https://haveibeenpwned.com/api/v2/breachedaccount/"+email,
+    }).done(function(json){data=json})
+      .fail(function(){alert("Failed for "+email)});
+    addHTML(data, email);
+  })
+}
+
+function addHTML(data, email) {
+  buildEmailDiv(email);
   $.each(data, function(_, obj) {
-    var name = obj.Name;
-    buildScaffoldFor(name, obj.Domain);
+    var website = obj.Name;
+    buildScaffoldFor(website, obj.Domain, email);
     $.each(obj, function(klass, item) {
-      var selector = "#"+name+" ."+klass;
+      var selector = "#"+email+" ."+name+" ."+klass;
       if ($(selector).length) {
         $(selector).append(item)
       }
     })
   })
 }
-function buildScaffoldFor(name, domain) {
-  $div = $("<div>", {id: name, class: "panel panel-default panel-body"});
+
+function buildEmailDiv(email) {
+  $div = $("<div>", {id: email});
+  $emailTitle = $("<h1>", {text: email});
+  $div.append($emailTitle);
+  $("#body").append($div);
+}
+
+function buildScaffoldFor(website, domain, email) {
+  $div = $("<div>", {id: website, class: "panel panel-default panel-body"});
   $title = $("<h2>", {class: "Title"});
   $domain = $("<a>", {href: domain})
   $count = $("<p>", {class: "PwnCount", text: "# Breached accounts: ", id: "numBreach"});
   $description = $("<p>", {class: "Description"});
   $div.append($title.append($domain), $count, $description);
-  $("#body").append($div);
+  $("#"+email).append($div);
 }
