73608a8dd683c648050136d5436b515016e30679
toponeoftopone <nooblingis@gmail.com>
2016-06-20 19:48:59 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+$.ajax({
+  url: "https://haveibeenpwned.com/api/v2/breachedaccount/test@example.com",
+
+}).done(handleRes)
+  .fail(function(){alert("Failed")});
+
+function handleRes(data) {
+  $.each(data, function(_, obj) {
+    var name = obj.Name;
+    buildScaffoldFor(name, obj.Domain);
+    $.each(obj, function(klass, item) {
+      var selector = "#"+name+" ."+klass;
+      if ($(selector).length) {
+        $(selector).append(item)
+      }
+    })
+  })
+}
+function buildScaffoldFor(name, domain) {
+  $div = $("<div>", {id: name, class: "panel panel-default panel-body"});
+  $title = $("<h2>", {class: "Title"});
+  $domain = $("<a>", {href: domain})
+  $count = $("<li>", {class: "PwnCount"});
+  $description = $("<li>", {class: "Description"});
+  $div.append($title.append($domain), $count, $description);
+  $("#body").append($div);
+}
